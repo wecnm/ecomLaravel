25ba11179d0afb6e936cb3092c1f71a5ceedd393
Ares Yang <yyh.fanatic@gmail.com>
2016-04-05 11:12:29 -0400

    First Initial ecom

---
+++ laravelECom/database/migrations/2016_04_05_132232_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('product_id');
+            $table->string('product_name');
+            $table->string('product_desc');
+            $table->string('product_detail');
+            $table->integer('category_id')->unsigned();
+            $table->boolean('product_available');
+            $table->string('picture_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
---
+++ laravelECom/database/migrations/2016_04_05_143508_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('category_id');
+            $table->string('category_name');
+            $table->timestamps();
+        });
+
+        Schema::table('products', function ($table) {
+            $table->foreign('category_id')
+                  ->references('category_id')
+                  ->on('categories')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+}
